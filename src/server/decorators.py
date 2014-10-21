c9d0504a4030d8b0947cc091d44d67b606f7c81f
jenkins <yifan>
2014-10-21 23:06:06 +0800

    2014-10-21 23:06:06 automatic update

--- src/server/decorators.py
+++ src/server/decorators.py
@@ -109,7 +109,7 @@ def project_verified_exclude_get(func):
             return func(request, *args, **kwargs)
         else:
             try:
-                prj = ProjectFile.objects.get(pk=ObjectId(['pid']))
+                prj = ProjectFile.objects.get(pk=ObjectId(data['pid']))
             except ObjectDoesNotExist:
                 return HttpResponse("{'status':'error', 'reason':'cannot find a project matching given pid'}")
             else:
